ec459f4c47bd507e3615726a04ec53bd446808c1
Yolanda Ribas <yolanda.ribas@hotmail.com>
2021-05-27 15:05:13 -0500

    Fix formating

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from '../styles/Filter.module.css';
 
-const Filter = ({onChange}) => {
-    return <input placeholder='Buscar una película...' onChange={(event)=>{onChange(event.target.value)}} className={styles.searchBar}/> 
-    }
+const Filter = ({ onChange }) => {
+    return <input placeholder='Buscar una película...' onChange={(event) => { onChange(event.target.value) }} className={styles.searchBar} />
+}
 
 export default Filter
